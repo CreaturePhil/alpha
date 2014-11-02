4aea0e5aff657589a97b3d04b2065edf4bcaff61
Phil <birkal@outlook.com>
2014-11-01 19:29:52 -0700

    Update tests for production

--- test/app.js
+++ test/app.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 var app = require('../index.js');
 
-describe('GET /', function() {
+describe('GET / in development env', function() {
   it('should return 200 OK', function(done) {
     request(app)
       .get('/')
@@ -9,7 +9,25 @@ describe('GET /', function() {
   });
 });
 
-describe('GET /about', function() {
+describe('GET /about in development env', function() {
+  it('should return 404 Page Not Found', function(done) {
+    request(app)
+      .get('/about')
+      .expect(404, done);
+  });
+});
+
+app.set('env', 'production');
+
+describe('GET / in production env', function() {
+  it('should return 200 OK', function(done) {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+});
+
+describe('GET /about in production env', function() {
   it('should return 404 Page Not Found', function(done) {
     request(app)
       .get('/about')
