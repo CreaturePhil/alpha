7e1efbc04a1f001712835ff0934aa57611494e62
Phil <birkal@outlook.com>
2014-11-01 20:00:13 -0700

    Revert back tests

--- test/app.js
+++ test/app.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 var app = require('../index.js');
 
-describe('GET / in development env', function() {
+describe('GET /', function() {
   it('should return 200 OK', function(done) {
     request(app)
       .get('/')
@@ -9,25 +9,7 @@ describe('GET / in development env', function() {
   });
 });
 
-describe('GET /about in development env', function() {
-  it('should return 404 Page Not Found', function(done) {
-    request(app)
-      .get('/about')
-      .expect(404, done);
-  });
-});
-
-app.set('env', 'production');
-
-describe('GET / in production env', function() {
-  it('should return 200 OK', function(done) {
-    request(app)
-      .get('/')
-      .expect(200, done);
-  });
-});
-
-describe('GET /about in production env', function() {
+describe('GET /about', function() {
   it('should return 404 Page Not Found', function(done) {
     request(app)
       .get('/about')
